acbf01f6bc9acab1e312129fdf175d8f8a74408a
jareepit <janmarieareepitak@gmail.com>
2019-02-22 16:25:48 -0600

    Added DB and start to DAL

---
+++ WebMovieStore/Models/DataAccessLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMovieStore.Models
+{
+    public enum GenreTypes { COMEDY, ACTION, ADVENTURE, ANIMATION, DRAMA, MYSTERY, ROMANCE, SCIFI, THRILLER };
+
+    public class DataAccessLayer
+    {
+        public List<Movie> getMoviesByGenre(GenreTypes genre)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                List<Movie> movies = (from p in context.Movies1
+                                      where p.Genre == genre.ToString()
+                                      select p).ToList<Movie>();
+                return movies;
+            }
+
+        }
+    }
+}
