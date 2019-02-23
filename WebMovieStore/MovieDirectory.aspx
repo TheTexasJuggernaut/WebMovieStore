ef89667fcd9bb4496817899e9d5adff12b47c879
jareepit <janmarieareepitak@gmail.com>
2019-02-23 09:28:57 -0600

    Added table of image buttons

--- WebMovieStore/MovieDirectory.aspx
+++ WebMovieStore/MovieDirectory.aspx
@@ -10,17 +10,53 @@
     <form id="form1" runat="server">
         <div>
         </div>
-        Browse Popular Genres<p>
+        <!--Browse Popular Genres<p>
             <asp:ImageButton ID="ImageButton1" runat="server"
                 AlternateText="ImageButton 1"
                 ImageAlign="left"
                 ImageUrl="Image/Thumbs/hands-holding-plant-silhouette-vector-graphics_template_1430381726381N7E.jpg"
                 OnClick="ImageButton_Click" />
-        </p>
+        </p> -->
+
+         <table style="height: 900px; width: 1200px">
+                <tr align="center">
+                    <td>
+                        <asp:ImageButton ID="ImageBtn1" runat="server" Height="150px" Width="250px" />
+                    </td>
+                    <td>
+                        <asp:ImageButton ID="ImageBtn2" runat="server" Height="150px" Width="250px"  />
+                    </td>
+                    <td >
+                        <asp:ImageButton ID="ImageBtn3" runat="server" Height="150px" Width="250px"  />
+                    </td>
+                </tr>
+                <tr align="center">
+                    <td >
+                        <asp:ImageButton ID="ImageBtn4" runat="server" Height="150px" Width="250px" />
+                    </td>
+                    <td class="auto-style7">
+                        <asp:ImageButton ID="ImageBtn5" runat="server" Height="150px" Width="250px"/>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:ImageButton ID="ImageBtn6" runat="server" Height="150px" Width="250px" />
+                    </td>
+                </tr>
+                <tr align="center">
+                    <td ">
+                        <asp:ImageButton ID="ImageBtn7" runat="server" Height="150px" Width="250px"  />
+                    </td>
+                    <td>
+                        <asp:ImageButton ID="ImageBtn8" runat="server" Height="150px" Width="250px"  />
+                    </td>
+                    <td>
+                        <asp:ImageButton ID="ImageBtn9" runat="server" Height="150px" Width="250px"  />
+                    </td>
+                </tr>
+            </table>
     </form>
+   <!-- <hr />
     <hr />
     <hr />
-    <hr />
-    <hr />
+    <hr /> -->
 </body>
 </html>
--- WebMovieStore/MovieDirectory.aspx.designer.cs
+++ WebMovieStore/MovieDirectory.aspx.designer.cs
@@ -29,5 +29,86 @@ namespace WebMovieStore {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+        
+        /// <summary>
+        /// ImageBtn1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn1;
+        
+        /// <summary>
+        /// ImageBtn2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn2;
+        
+        /// <summary>
+        /// ImageBtn3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn3;
+        
+        /// <summary>
+        /// ImageBtn4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn4;
+        
+        /// <summary>
+        /// ImageBtn5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn5;
+        
+        /// <summary>
+        /// ImageBtn6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn6;
+        
+        /// <summary>
+        /// ImageBtn7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn7;
+        
+        /// <summary>
+        /// ImageBtn8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn8;
+        
+        /// <summary>
+        /// ImageBtn9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageBtn9;
     }
 }
