d679645ce8e7de9b65e0c777578872b360c00896
jareepit <janmarieareepitak@gmail.com>
2019-02-23 09:15:32 -0600

    Added more DAL methods

--- WebMovieStore/Models/DataAccessLayer.cs
+++ WebMovieStore/Models/DataAccessLayer.cs
@@ -20,5 +20,70 @@ namespace WebMovieStore.Models
             }
 
         }
+
+        public int addNewOrder(Order newOrder)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                context.Orders.Add(newOrder);
+                context.SaveChanges();
+
+                return newOrder.Id;
+            }
+        }
+
+        public void addOrderItem(OrderItem orderItem)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                context.OrderItems.Add(orderItem);
+                context.SaveChanges();
+
+            }
+        }
+
+        public Customer getCustomerByUsername(string username)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                Customer customer = (from c in context.Customers
+                                     where c.Name == username
+                                     select c).FirstOrDefault<Customer>();
+                return customer;
+            }
+        }
+
+        public Movie getMovieById(int movieId)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                Movie movie = (from c in context.Movies1
+                               where c.Id == movieId
+                               select c).FirstOrDefault<Movie>();
+                return movie;
+            }
+        }
+
+        public Order getOrderById(int orderId)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                Order order = (from c in context.Orders
+                               where c.Id == orderId
+                               select c).FirstOrDefault<Order>();
+                return order;
+            }
+        }
+
+        public OrderItem getOrderItemById(int orderId)
+        {
+            using (var context = new MovieStoreEDM())
+            {
+                OrderItem orderItem = (from c in context.OrderItems
+                                       where c.Id == orderId
+                                       select c).FirstOrDefault<OrderItem>();
+                return orderItem;
+            }
+        }
     }
 }
