e47da0b06ac9c1007d9e9fc20a669fb4643f0cd9
TheTexasJuggernaut <43121927+TheTexasJuggernaut@users.noreply.github.com>
2019-02-24 11:48:49 -0600

    Login Work

--- WebMovieStore/HomePage.aspx.cs
+++ WebMovieStore/HomePage.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebMovieStore.Models;
 
 namespace WebMovieStore
 {
     public partial class HomePage : System.Web.UI.Page
     {
+       Customer List = new Customer();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +18,25 @@ namespace WebMovieStore
 
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
+            String field = Login1.UserName.ToString();
+            String pass = Login1.Password.ToString();
+           bool work = List.Name.Contains(field);
+                
+        }
 
+        protected void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
